c189ca5bc679bdab89c129108116e779dfc23fb3
Angelawoo572 <angelawoo571@gmail.com>
2025-07-07 19:07:58 -0400

    Add files via upload

--- code.py
+++ code.py
@@ -27,19 +27,20 @@ import busio
 # input output packages
 import digitalio
 
-import adafruit_ds3231
+# import adafruit_ds3231
 
 # address constants
 TMP_ADDR = 0x48
 BTN_ADDR = 0x6f
 LCD_ADDR = 0x72
+RTC_ADDR = 0x68
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 #               scl0(GP5)      sda0(GP4)      100kHz
 i2c = busio.I2C(scl=board.GP5, sda=board.GP4, frequency=100000)
-rtc = adafruit_ds3231.DS3231(i2c)
+# rtc = adafruit_ds3231.DS3231(i2c)
 
 # @brief scan the I2C line and return a list of all addresses which respond
 # @return list of addresses which respond to I2C prompt
@@ -49,6 +50,52 @@ def checkDevices():
     l = i2c.scan()
     i2c.unlock()
     return l
+
+# @brief    Convert BCD (Binary-Coded Decimal) to decimal
+# @return   equivalent decimal integer (0–99)
+def bcd_to_decimal(bcd):
+    return (bcd >> 4) * 10 + (bcd & 0x0F)
+
+def readClock():
+    while not i2c.try_lock():
+        time.sleep(0.1)
+    data = bytearray(7)
+    buf = bytearray(1)
+    buf[0] = 0x00
+    i2c.writeto(RTC_ADDR, buf)
+    i2c.readfrom_into(RTC_ADDR, data)
+    i2c.unlock()
+
+    second = bcd_to_decimal(data[0])
+    minute = bcd_to_decimal(data[1])
+    hour   = bcd_to_decimal(data[2] & 0x3F)
+    day    = bcd_to_decimal(data[4])
+    month  = bcd_to_decimal(data[5] & 0x1F)
+    year   = bcd_to_decimal(data[6]) + 2000
+
+    return time.struct_time((year, month, day, hour, minute, second, 0, -1, -1))
+
+def decimal_to_bcd(dec):
+    return ((dec // 10) << 4) | (dec % 10)
+
+def setClock(year, month, day, hour, minute, second):
+    while not i2c.try_lock():
+        time.sleep(0.1)
+
+    # DS3231 从 0x00 开始，依次是 sec, min, hr, weekday, date, month, year
+    data = bytearray(8)
+    data[0] = 0x00  # 起始寄存器地址
+    data[1] = decimal_to_bcd(second)
+    data[2] = decimal_to_bcd(minute)
+    data[3] = decimal_to_bcd(hour)
+    data[4] = 1  # weekday 1=Monday，可随便设
+    data[5] = decimal_to_bcd(day)
+    data[6] = decimal_to_bcd(month)
+    data[7] = decimal_to_bcd(year - 2000)  # 只写后两位
+
+    i2c.writeto(RTC_ADDR, data)
+    i2c.unlock()
+
 
 # @brief    read temperature in Celsius as a floating point number
 # @return   temperature in Celsius as a floating point number
@@ -106,21 +153,21 @@ def clearLCD():
 # @brief        print stuff to the LCD
 # @param[in]    pressed - whether the button is pressed or not
 # @param[in]    temp - current temperature in Celsius as a floating point number
+# @param[in] now      time.struct_time returned by readClock()
 # @return       whether or not the button has been pressed
 def printLCD(pressed, temp,now):
     # TODO: implement printLCD, remove pass if implemented
     while not i2c.try_lock():
         time.sleep(0.1)
 
-    time_str = f"{now.tm_hour:02}:{now.tm_min:02}:{now.tm_sec:02}"
-
     if pressed == True:
-        msg = f" hello, temp = {temp:.1f}"
+        msg1 = f"{now.tm_hour:02}:{now.tm_min:02}:{now.tm_sec:02}"
     else:
-        msg = f"  bye, temp = {temp:.1f}"
-    full = f"{time_str}\n{msg}"
+        msg1 = f"{now.tm_year}-{now.tm_mon:02}-{now.tm_mday:02}"
+    msg2 = f"Temp = {temp:.1f}C"
+    full = f"{msg1} {msg2}"
 
-    i2c.writeto(LCD_ADDR, full.encode("utf-8"))
+    i2c.writeto(LCD_ADDR, full)
 
     i2c.unlock()
     return pressed
@@ -144,12 +191,14 @@ def setBackLight(r, g, b):
 
 
 lightCounter = 0
+# setClock(2025, 7, 7, 19, 07, 0)
 while True:
     # uncomment this to check which devices are connected
     # print([hex(i) for i in checkDevices()])
     temp = readTemp()
     pressed = readBtnStatus()
-    now = rtc.datetime
+    # now = rtc.datetime
+    now = readClock()
     print(f"Current time: {now.tm_hour:02}:{now.tm_min:02}:{now.tm_sec:02}")
 
     # inserts return value of readTemp() into "It's a lovely {} C today!" and prints
